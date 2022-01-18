f9460f02a934445630f1ad03768eb9b7bd80d765
ecell-xie <97974449+ecell-xie@users.noreply.github.com>
2022-01-18 22:15:20 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby5IcHJ5f2ZFmA3ico_xCytGJ9EyEKlbnNjy--xE5wXlFSGdo-80LkOFFXb2Ft2L1w8/exec";
+const form = document.forms["google-sheet"];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => alert("Response Saved"))
+    .catch((error) => console.error("error", error.message));
+});
